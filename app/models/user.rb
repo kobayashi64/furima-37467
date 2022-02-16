a5017109e939a0f392f4651af2c2dc97730f5352
kobayashi64 <youkoba64@gmail.com>
2022-02-16 17:46:41 +0900

    validates設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+  validates :family_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' }
+  validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' }
+  validates :family_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カナ文字を使用してください' }
+  validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カナ文字を使用してください' }           
+  validates :nickname
+  validates :birthday  
+  end
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'は半角英数を両方含む必要があります'}  
+  validates :email, uniqueness: true 
+  
 end
