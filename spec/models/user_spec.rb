71f87c8786768e44cf0dd2d1ffdcc774056766ca
kobayashi64 <youkoba64@gmail.com>
2022-02-17 11:19:52 +0900

    テストコードの修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -21,25 +21,25 @@ RSpec.describe User, type: :model do
       it 'family_nameが空では登録できない' do
         @user.family_name = ''
         @user.valid?
-        expect(@user.errors.full_messages).to include("Family name can't be blank", 'Family name 全角文字を使用してください')
+        expect(@user.errors.full_messages).to include("Family name can't be blank")
       end
 
       it 'first_nameが空では登録できない' do
         @user.first_name = ''
         @user.valid?
-        expect(@user.errors.full_messages).to include("First name can't be blank", 'First name 全角文字を使用してください')
+        expect(@user.errors.full_messages).to include("First name can't be blank")
       end
 
       it 'first_name_kanaが空では登録できない' do
         @user.first_name_kana = ''
         @user.valid?
-        expect(@user.errors.full_messages).to include("First name kana can't be blank", 'First name kana 全角カナ文字を使用してください')
+        expect(@user.errors.full_messages).to include("First name kana can't be blank")
       end
 
       it 'family_name_kanaが空では登録できない' do
         @user.family_name_kana = ''
         @user.valid?
-        expect(@user.errors.full_messages).to include("Family name kana can't be blank", 'Family name kana 全角カナ文字を使用してください')
+        expect(@user.errors.full_messages).to include("Family name kana can't be blank")
       end
 
       it 'family_nameが全角（漢字・ひらがな・カタカナ）でないと登録できない' do
