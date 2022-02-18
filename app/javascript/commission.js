93a24c386c723db06ed39e434ea754b8d5008e58
kobayashi64 <youkoba64@gmail.com>
2022-02-18 17:26:31 +0900

    javascript販売手数料、販売利益の表示

---
+++ app/javascript/commission.js
@@ -0,0 +1,14 @@
+function count (){
+  const price  = document.getElementById("item-price");
+  price.addEventListener("keyup", () => {
+    const countVal = Math.floor(price.value * 0.1)
+    const commissionNum  = document.getElementById("add-tax-price");
+    commissionNum.innerHTML =  countVal
+
+    const profitVal = price.value - countVal
+    const profitNum  = document.getElementById("profit");
+    profitNum.innerHTML =  profitVal
+  });
+};
+
+window.addEventListener('load', count);
