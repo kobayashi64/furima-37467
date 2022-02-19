355437c9caf7ad34dc30586a8867c778afed0b96
kobayashi64 <youkoba64@gmail.com>
2022-02-19 21:45:05 +0900

    商品削除機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :move_to_signed_in, only: [:edit, :new]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
     @items = Item.all.order(created_at: :desc)
@@ -31,6 +31,13 @@ class ItemsController < ApplicationController
       redirect_to item_path(@item.id)
     else
       render :edit
+    end
+  end
+
+  def destroy
+    if user_signed_in? && @item.user_id == current_user.id
+      @item.destroy
+      redirect_to root_path
     end
   end
 
