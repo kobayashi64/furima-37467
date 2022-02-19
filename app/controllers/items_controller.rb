6b78ef924cca036a1007676cfaaf568daf186a3e
kobayashi64 <youkoba64@gmail.com>
2022-02-19 20:40:40 +0900

    itemコントローラの記載修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :move_to_signed_in, only: [:edit]
+  before_action :move_to_signed_in, only: [:edit, :new]
+  before_action :set_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.all.order(created_at: :desc)
@@ -7,7 +8,6 @@ class ItemsController < ApplicationController
 
   def new
     @item = Item.new
-    redirect_to user_session_path unless user_signed_in?
   end
 
   def create
@@ -20,27 +20,18 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
-
-    redirect_to root_path unless user_signed_in? && @item.user_id == current_user.id
+    redirect_to root_path unless @item.user_id == current_user.id
   end
 
   def update
-    @item = Item.find(params[:id])
-    @item.update(item_params)
-    if @item.save
-      redirect_to item_path
+    if @item.update(item_params)
+      redirect_to item_path(@item.id)
     else
       render :edit
     end
-  end
-
-  def create
-    @item = Item.new(item_params)
   end
 
   private
@@ -52,5 +43,9 @@ class ItemsController < ApplicationController
 
   def move_to_signed_in
     redirect_to user_session_path unless user_signed_in?
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
